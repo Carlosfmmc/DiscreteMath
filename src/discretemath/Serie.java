94d6a24bd8fdf84be3c2c70399a14ab7725e42e4
CarlosFmmc <CarlosFmmc@DESKTOP-DEUM11Q.home>
2019-10-04 02:09:43 +0100

    Serie with not static Iterator

--- src/discretemath/Serie.java
+++ src/discretemath/Serie.java
@@ -5,14 +5,41 @@ import java.util.Iterator;
 class Serie implements Iterable<Integer>{
     
     private RecursiveFunction r;
+    private int maxIteration = -1;
+    private int n = 0;
     
     public Serie(RecursiveFunction r) {
-        r = r;
+        this.r = r;
     }
 
+    public Serie(RecursiveFunction r, int maxIteration) {
+        this.r = r;
+        this.maxIteration = maxIteration;
+    }
+
+    public int getMaxIteration() {
+        return maxIteration;
+    }
+
+    public void setMaxIteration(int maxIteration) {
+        this.maxIteration = maxIteration;
+    }
+        
     @Override
     public Iterator iterator() {
-        
+        return i;
     }
+    private Iterator<Integer> i = new Iterator<Integer>() {
+
+        @Override
+        public boolean hasNext() {
+            return n != maxIteration;
+        }
+
+        @Override
+        public Integer next() {
+            return r.getN(++n);
+        }
+    };
     
 }
